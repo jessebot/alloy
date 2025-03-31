ce15562a908382e02ee1b25fc91cf0f8e323d0ee
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2025-03-31 17:33:03 +0200

    wip

--- internal/component/database_observability/mysql/collector/query_sample_test.go
+++ internal/component/database_observability/mysql/collector/query_sample_test.go
@@ -3,6 +3,7 @@ package collector
 import (
 	"database/sql/driver"
 	"fmt"
+	"math"
 	"os"
 	"testing"
 	"time"
@@ -10,6 +11,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-kit/log"
 	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 
@@ -29,12 +31,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "select query",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -55,12 +54,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "truncated query",
 			rows: [][]driver.Value{{
-				"3",
-				"4",
 				"some_schema",
 				"some_digest",
 				"insert into some_table (`id1`, `id2`, `id3`, `id...",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -71,12 +67,9 @@ func TestQuerySample(t *testing.T) {
 				"456",
 				"457",
 			}, {
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -97,12 +90,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "truncated in multi-line comment",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1 /*traceparent='00-abc...",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -124,12 +114,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "truncated with properly closed comment",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1 /* comment that's closed */ and name = 'test...",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -146,12 +133,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "start transaction",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"START TRANSACTION",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -172,12 +156,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "sql parse error",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -188,12 +169,9 @@ func TestQuerySample(t *testing.T) {
 				"456",
 				"457",
 			}, {
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"not valid sql",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -214,12 +192,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "multiple schemas",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"select * from some_table where id = 1",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -230,12 +205,9 @@ func TestQuerySample(t *testing.T) {
 				"456",
 				"457",
 			}, {
-				"1",
-				"2",
 				"some_other_schema",
 				"some_digest",
 				"select * from some_table where id = 1",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -258,12 +230,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "subquery and union",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"SELECT * FROM (SELECT id, name FROM employees_us_east UNION SELECT id, name FROM employees_us_west) as employees_us UNION SELECT id, name FROM employees_emea",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -284,12 +253,9 @@ func TestQuerySample(t *testing.T) {
 		{
 			name: "show create table (table name is not parsed)",
 			rows: [][]driver.Value{{
-				"1",
-				"2",
 				"some_schema",
 				"some_digest",
 				"SHOW CREATE TABLE some_table",
-				"50000000",
 				"70000000",
 				"20000000",
 				"10000000",
@@ -329,24 +295,34 @@ func TestQuerySample(t *testing.T) {
 			require.NoError(t, err)
 			require.NotNil(t, collector)
 
-			mock.ExpectQuery(selectLatestTimerEnd).WithoutArgs().RowsWillBeClosed().
+			mock.ExpectQuery(selectUptime).WithoutArgs().RowsWillBeClosed().
 				WillReturnRows(
 					sqlmock.NewRows([]string{
-						"timer_end",
+						"uptime",
 					}).AddRow(
-						"1",
+						"1", // corresponds to initial timerBookmark
 					),
 				)
 
-			mock.ExpectQuery(selectQuerySamples).WithArgs(float64(1)).RowsWillBeClosed().
+			mock.ExpectQuery(selectNowAndUptime).WithoutArgs().WillReturnRows(
+				sqlmock.NewRows([]string{
+					"now",
+					"uptime",
+				}).AddRow(
+					1,
+					2,
+				),
+			)
+
+			mock.ExpectQuery(selectQuerySamples+endOfTimeline).WithArgs(
+				1e12, // initial timerBookmark
+				2e12,
+			).RowsWillBeClosed().
 				WillReturnRows(
 					sqlmock.NewRows([]string{
-						"now",
-						"uptime",
 						"statements.CURRENT_SCHEMA",
 						"statements.DIGEST",
 						"statements.DIGEST_TEXT",
-						"statements.TIMER_START",
 						"statements.TIMER_END",
 						"statements.TIMER_WAIT",
 						"statements.CPU_TIME",
@@ -388,133 +364,6 @@ func TestQuerySample(t *testing.T) {
 	}
 }
 
-func TestQuerySampleUpdatesLastSampleSeenTimestamp(t *testing.T) {
-	defer goleak.VerifyNone(t)
-	t.Run("fetch new samples after the last sample seen timestamp", func(t *testing.T) {
-		t.Parallel()
-
-		db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
-		require.NoError(t, err)
-		defer db.Close()
-
-		lokiClient := loki_fake.NewClient(func() {})
-
-		collector, err := NewQuerySample(QuerySampleArguments{
-			DB:              db,
-			InstanceKey:     "mysql-db",
-			CollectInterval: time.Second,
-			EntryHandler:    lokiClient,
-			Logger:          log.NewLogfmtLogger(os.Stderr),
-		})
-		require.NoError(t, err)
-		require.NotNil(t, collector)
-
-		mock.ExpectQuery(selectLatestTimerEnd).WithoutArgs().RowsWillBeClosed().
-			WillReturnRows(
-				sqlmock.NewRows([]string{
-					"timer_end",
-				}).AddRow(
-					"1",
-				))
-
-		mock.ExpectQuery(selectQuerySamples).WithArgs(float64(1)).RowsWillBeClosed().
-			WillReturnRows(
-				sqlmock.NewRows([]string{
-					"now",
-					"uptime",
-					"statements.CURRENT_SCHEMA",
-					"statements.DIGEST",
-					"statements.DIGEST_TEXT",
-					"statements.TIMER_START",
-					"statements.TIMER_END",
-					"statements.TIMER_WAIT",
-					"statements.CPU_TIME",
-					"statements.ROWS_EXAMINED",
-					"statements.ROWS_SENT",
-					"statements.ROWS_AFFECTED",
-					"statements.ERRORS",
-					"statements.MAX_CONTROLLED_MEMORY",
-					"statements.MAX_TOTAL_MEMORY",
-				}).AddRow(
-					"1",
-					"2",
-					"some_schema",
-					"some_digest",
-					"select * from some_table where id = 1",
-					"50000000",
-					"70000000",
-					"20000000",
-					"10000000",
-					"5",
-					"5",
-					"0",
-					"0",
-					"456",
-					"457",
-				),
-			)
-
-		mock.ExpectQuery(selectQuerySamples).WithArgs(float64(70000000)).RowsWillBeClosed().
-			WillReturnRows(
-				sqlmock.NewRows([]string{
-					"now",
-					"uptime",
-					"statements.CURRENT_SCHEMA",
-					"statements.DIGEST",
-					"statements.DIGEST_TEXT",
-					"statements.TIMER_START",
-					"statements.TIMER_END",
-					"statements.TIMER_WAIT",
-					"statements.CPU_TIME",
-					"statements.ROWS_EXAMINED",
-					"statements.ROWS_SENT",
-					"statements.ROWS_AFFECTED",
-					"statements.ERRORS",
-					"statements.MAX_CONTROLLED_MEMORY",
-					"statements.MAX_TOTAL_MEMORY",
-				}).AddRow(
-					"1",
-					"2",
-					"some_schema",
-					"some_digest",
-					"select * from some_table where id = 1",
-					"50000000",
-					"70000000",
-					"20000000",
-					"10000000",
-					"5",
-					"5",
-					"0",
-					"0",
-					"456",
-					"457",
-				),
-			)
-
-		err = collector.Start(t.Context())
-		require.NoError(t, err)
-
-		require.Eventually(t, func() bool {
-			return len(lokiClient.Received()) == 2
-		}, 5*time.Second, 100*time.Millisecond)
-
-		collector.Stop()
-		lokiClient.Stop()
-
-		require.Eventually(t, func() bool {
-			return collector.Stopped()
-		}, 5*time.Second, 100*time.Millisecond)
-
-		err = mock.ExpectationsWereMet()
-		require.NoError(t, err)
-
-		lokiEntries := lokiClient.Received()
-		require.Equal(t, model.LabelSet{"job": database_observability.JobName, "op": OP_QUERY_SAMPLE, "instance": "mysql-db"}, lokiEntries[0].Labels)
-		require.Equal(t, `schema="some_schema" digest="some_digest" digest_text="select * from some_table where id = :redacted1" rows_examined="5" rows_sent="5" rows_affected="0" errors="0" max_controlled_memory="456b" max_total_memory="457b" cpu_time="0.010000ms" elapsed_time="0.020000ms" elapsed_time_ms="0.020000ms"`, lokiEntries[0].Line)
-		require.Equal(t, `schema="some_schema" digest="some_digest" digest_text="select * from some_table where id = :redacted1" rows_examined="5" rows_sent="5" rows_affected="0" errors="0" max_controlled_memory="456b" max_total_memory="457b" cpu_time="0.010000ms" elapsed_time="0.020000ms" elapsed_time_ms="0.020000ms"`, lokiEntries[0].Line)
-	})
-}
-
 func TestQuerySampleSQLDriverErrors(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
@@ -537,15 +386,28 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, collector)
 
-		mock.ExpectQuery(selectLatestTimerEnd).WithoutArgs().RowsWillBeClosed().
+		mock.ExpectQuery(selectUptime).WithoutArgs().RowsWillBeClosed().
 			WillReturnRows(
 				sqlmock.NewRows([]string{
-					"timer_end",
+					"uptime",
 				}).AddRow(
 					"1",
 				))
 
-		mock.ExpectQuery(selectQuerySamples).WithArgs(float64(1)).RowsWillBeClosed().
+		mock.ExpectQuery(selectNowAndUptime).WithoutArgs().WillReturnRows(
+			sqlmock.NewRows([]string{
+				"now",
+				"uptime",
+			}).AddRow(
+				1,
+				2,
+			),
+		)
+
+		mock.ExpectQuery(selectQuerySamples+endOfTimeline).WithArgs(
+			1e12, // initial timerBookmark
+			2e12,
+		).RowsWillBeClosed().
 			WillReturnRows(
 				sqlmock.NewRows([]string{
 					"digest", // not enough columns
@@ -553,15 +415,25 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 					"abc123",
 				))
 
-		mock.ExpectQuery(selectQuerySamples).WithArgs(float64(1)).RowsWillBeClosed().
+		mock.ExpectQuery(selectNowAndUptime).WithoutArgs().WillReturnRows(
+			sqlmock.NewRows([]string{
+				"now",
+				"uptime",
+			}).AddRow(
+				1,
+				2,
+			),
+		)
+
+		mock.ExpectQuery(selectQuerySamples+endOfTimeline).WithArgs(
+			2e12, // initial timerBookmark
+			2e12,
+		).RowsWillBeClosed().
 			WillReturnRows(
 				sqlmock.NewRows([]string{
-					"now",
-					"uptime",
 					"statements.CURRENT_SCHEMA",
 					"statements.DIGEST",
 					"statements.DIGEST_TEXT",
-					"statements.TIMER_START",
 					"statements.TIMER_END",
 					"statements.TIMER_WAIT",
 					"statements.CPU_TIME",
@@ -572,12 +444,9 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 					"statements.MAX_CONTROLLED_MEMORY",
 					"statements.MAX_TOTAL_MEMORY",
 				}).AddRow(
-					"1",
-					"2",
 					"some_schema",
 					"some_digest",
 					"select * from some_table where id = 1",
-					"50000000",
 					"70000000",
 					"20000000",
 					"10000000",
@@ -631,10 +500,10 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, collector)
 
-		mock.ExpectQuery(selectLatestTimerEnd).WithoutArgs().RowsWillBeClosed().
+		mock.ExpectQuery(selectUptime).WithoutArgs().RowsWillBeClosed().
 			WillReturnRows(
 				sqlmock.NewRows([]string{
-					"timer_end",
+					"uptime",
 				}).AddRow(
 					"1",
 				))
@@ -733,10 +602,10 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, collector)
 
-		mock.ExpectQuery(selectLatestTimerEnd).WithoutArgs().RowsWillBeClosed().
+		mock.ExpectQuery(selectUptime).WithoutArgs().RowsWillBeClosed().
 			WillReturnRows(
 				sqlmock.NewRows([]string{
-					"timer_end",
+					"uptime",
 				}).AddRow(
 					"1",
 				))
@@ -799,5 +668,41 @@ func TestQuerySampleSQLDriverErrors(t *testing.T) {
 		lokiEntries := lokiClient.Received()
 		require.Equal(t, model.LabelSet{"job": database_observability.JobName, "op": OP_QUERY_SAMPLE, "instance": "mysql-db"}, lokiEntries[0].Labels)
 		require.Equal(t, `schema="some_schema" digest="some_digest" digest_text="select * from some_table where id = :redacted1" rows_examined="5" rows_sent="5" rows_affected="0" errors="0" max_controlled_memory="456b" max_total_memory="457b" cpu_time="0.010000ms" elapsed_time="0.020000ms" elapsed_time_ms="0.020000ms"`, lokiEntries[0].Line)
+	})
+}
+
+func TestQuerySampleSummary_initializeTimer(t *testing.T) {
+	t.Run("selects uptime, sets timerBookmark", func(t *testing.T) {
+		db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		require.NoError(t, err)
+		defer db.Close()
+
+		mock.ExpectQuery(selectUptime).WithoutArgs().WillReturnRows(sqlmock.NewRows([]string{
+			"uptime",
+		}).AddRow(
+			5,
+		))
+
+		c := &QuerySample{dbConnection: db}
+		require.NoError(t, c.initializeBookmark(t.Context()))
+
+		assert.Equal(t, 5e12, c.timerBookmark)
+	})
+
+	t.Run("sets timerBookmark as uptime modulo overflows (uptime is comprised of 1 overflow)", func(t *testing.T) {
+		db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		require.NoError(t, err)
+		defer db.Close()
+
+		mock.ExpectQuery(selectUptime).WithoutArgs().WillReturnRows(sqlmock.NewRows([]string{
+			"uptime",
+		}).AddRow(
+			picosecondsToSeconds(math.MaxUint64) + 5,
+		))
+
+		c := &QuerySample{dbConnection: db}
+		require.NoError(t, c.initializeBookmark(t.Context()))
+
+		assert.Equal(t, 5e12, c.timerBookmark)
 	})
 }
